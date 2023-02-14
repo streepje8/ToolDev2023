34834e441f9f3ded5be5e2c68d920b1bf5db1d15
streepje8 <wessel@wezzel.nl>
2023-02-14 17:12:44 +0100

    Buggy camera controls

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,6 +1,40 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Preview preview;
+    public float sensitifity;
+
+    private bool inDrag = false;
+    private Vector3 dragStartPos;
+    private Quaternion startRotation;
     
+    private void Awake()
+    {
+        if (preview == null)
+        {
+            Debug.LogWarning("Skill Issue: Preview of CameraController is unassigned!");
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (preview.isBeeingHovered)
+        {
+            if (Input.GetMouseButton(0) && !inDrag)
+            {
+                dragStartPos = Input.mousePosition;
+                startRotation = transform.rotation;
+                inDrag = true;
+            }
+        }
+        if (inDrag)
+        {
+            Vector3 delta = dragStartPos - Input.mousePosition;
+            transform.rotation = startRotation * Quaternion.Euler(new Vector3(delta.y,delta.x,0) * (sensitifity * Time.deltaTime));
+            if (!Input.GetMouseButton(0)) inDrag = false;
+        }
+    }
 }
--- Assets/Preview.cs
+++ Assets/Preview.cs
@@ -3,13 +3,14 @@ using UnityEngine.EventSystems;
 
 public class Preview : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
 {
+    public bool isBeeingHovered = false;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        isBeeingHovered = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        isBeeingHovered = false;
     }
 }
