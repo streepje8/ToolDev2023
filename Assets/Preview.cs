89a9b8335b4c80766631f73c4621e040c311b28c
streepje8 <wessel@wezzel.nl>
2023-02-14 15:56:29 +0100

    Initial Commit

---
+++ Assets/Preview.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Preview : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
